4f27001acca7c678688f41ccab2f15cf9c2aba5f
Zed <124834187+zZedix@users.noreply.github.com>
2025-09-24 23:58:30 +0330

    Update script.sh

--- script.sh
+++ script.sh
@@ -15,9 +15,6 @@ mkdir -p /var/lib/pasarguard/mysql
 [ -f /opt/pasarguard/docker-compose.yml ] && sed -i 's|image: *gozargah/marzban:latest|image: pasarguard/panel:latest|' /opt/pasarguard/docker-compose.yml
 [ -f /opt/pasarguard/docker-compose.yml ] && sed -i 's|- /var/lib/marzban:/var/lib/marzban|- /var/lib/pasarguard:/var/lib/pasarguard|' /opt/pasarguard/docker-compose.yml
 
-# If the Marzban binary exists, rename it to pasarguard
-[ -f /usr/local/bin/marzban ] && mv /usr/local/bin/marzban /usr/local/bin/pasarguard
-
 pasarguard update
 pasarguard restart
 
