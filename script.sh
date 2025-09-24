4478013acd470c667f36a9cf43afe06f9d46c695
Zed <124834187+zZedix@users.noreply.github.com>
2025-09-24 23:24:31 +0330

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,7 +13,7 @@ cp -a /var/lib/marzban/mysql/. /var/lib/pasarguard/mysql/
 cp -a /var/lib/marzban/mysql-config /var/lib/pasarguard/
 cp -a /var/lib/marzban/xray-config.json /var/lib/pasarguard/
 
-sed -i 's|image: *gozargah/marzban:latest|image: pasarguard/panel:latest|' /opt/pasarguard/docker-compose.yml
-sed -i 's|- /var/lib/marzban:/var/lib/marzban|- /var/lib/pasarguard:/var/lib/pasarguard|' /opt/pasarguard/docker-compose.yml
+sed -i 's|image: *gozargah/marzban:latest|image: pasarguard/panel:next|' /opt/pasarguard/docker-compose.yml
+sed -i 's|- /var/lib/marzban:/var/lib/marzban|- pasarguard/panel:next|' /opt/pasarguard/docker-compose.yml
 
 yes | marzban uninstall
