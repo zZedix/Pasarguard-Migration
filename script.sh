62a8ffa78b724f6db956a5aec76507447709c430
Zed <124834187+zZedix@users.noreply.github.com>
2025-09-24 23:53:05 +0330

    Update script.sh

--- script.sh
+++ script.sh
@@ -12,8 +12,11 @@ mkdir -p /var/lib/pasarguard/mysql
 [ -d /var/lib/marzban/mysql-config ] && cp -a /var/lib/marzban/mysql-config /var/lib/pasarguard/
 [ -f /var/lib/marzban/xray-config.json ] && cp -a /var/lib/marzban/xray-config.json /var/lib/pasarguard/
 
-[ -f /opt/pasarguard/docker-compose.yml ] && sed -i 's|image: *gozargah/marzban:latest|image: pasarguard/panel:next|' /opt/pasarguard/docker-compose.yml
+[ -f /opt/pasarguard/docker-compose.yml ] && sed -i 's|image: *gozargah/marzban:latest|image: pasarguard/panel:latest|' /opt/pasarguard/docker-compose.yml
 [ -f /opt/pasarguard/docker-compose.yml ] && sed -i 's|- /var/lib/marzban:/var/lib/marzban|- /var/lib/pasarguard:/var/lib/pasarguard|' /opt/pasarguard/docker-compose.yml
+
+# If the Marzban binary exists, rename it to pasarguard
+[ -f /usr/local/bin/marzban ] && mv /usr/local/bin/marzban /usr/local/bin/pasarguard
 
 pasarguard update
 pasarguard restart
