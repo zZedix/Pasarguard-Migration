61cc1b0cf360b3dad85f8842ed755117aa3ec8bd
Zed <124834187+zZedix@users.noreply.github.com>
2025-09-24 23:41:25 +0330

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,16 +5,15 @@ mkdir -p /opt/pasarguard
 mkdir -p /var/lib/pasarguard/certs
 mkdir -p /var/lib/pasarguard/mysql
 
-cp -f /opt/marzban/docker-compose.yml /opt/pasarguard/docker-compose.yml
-cp -f /opt/marzban/.env /opt/pasarguard/.env
+[ -f /opt/marzban/docker-compose.yml ] && cp -f /opt/marzban/docker-compose.yml /opt/pasarguard/docker-compose.yml
+[ -f /opt/marzban/.env ] && cp -f /opt/marzban/.env /opt/pasarguard/.env
+[ -d /var/lib/marzban/certs ] && cp -a /var/lib/marzban/certs/. /var/lib/pasarguard/certs/
+[ -d /var/lib/marzban/mysql ] && cp -a /var/lib/marzban/mysql/. /var/lib/pasarguard/mysql/
+[ -d /var/lib/marzban/mysql-config ] && cp -a /var/lib/marzban/mysql-config /var/lib/pasarguard/
+[ -f /var/lib/marzban/xray-config.json ] && cp -a /var/lib/marzban/xray-config.json /var/lib/pasarguard/
 
-cp -a /var/lib/marzban/certs/. /var/lib/pasarguard/certs/
-cp -a /var/lib/marzban/mysql/. /var/lib/pasarguard/mysql/
-cp -a /var/lib/marzban/mysql-config /var/lib/pasarguard/
-cp -a /var/lib/marzban/xray-config.json /var/lib/pasarguard/
-
-sed -i 's|image: *gozargah/marzban:latest|image: pasarguard/panel:latest|' /opt/pasarguard/docker-compose.yml
-sed -i 's|- /var/lib/marzban:/var/lib/marzban|- /var/lib/pasarguard:/var/lib/pasarguard|' /opt/pasarguard/docker-compose.yml
+[ -f /opt/pasarguard/docker-compose.yml ] && sed -i 's|image: *gozargah/marzban:latest|image: pasarguard/panel:latest|' /opt/pasarguard/docker-compose.yml
+[ -f /opt/pasarguard/docker-compose.yml ] && sed -i 's|- /var/lib/marzban:/var/lib/marzban|- /var/lib/pasarguard:/var/lib/pasarguard|' /opt/pasarguard/docker-compose.yml
 
 pasarguard update
 pasarguard restart
